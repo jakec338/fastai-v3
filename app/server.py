58f3c5e251d38a5559195b4acb3fa5ebad2f9152
Ubuntu <jakec338@FastAI.t4rkou0vlutulnrquy3jbxbuga.fx.internal.cloudapp.net>
2019-11-14 13:15:21 +0000

    change model path

--- app/server.py
+++ app/server.py
@@ -9,10 +9,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/gfeg4tknlagf2gi/export_v3.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+
+classes = ['apples', 'bananas', 'broccoli', 'leeks', 'onions', 'oranges', 'potatos', 'tomatos']
+
 path = Path(__file__).parent
 
 app = Starlette()
